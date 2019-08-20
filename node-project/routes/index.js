2ce4d4a133c9c0a0db6ac67b9d8221477bfe5b3e
ohhhleg <1307049434@qq.com>
2019-08-20 17:37:54 +0800

    node.js

---
+++ node-project/routes/index.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var router = express.Router();
+const {
+  find,
+  plus
+} = require('../lib/db');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/*插入*/
+router.post('/plusProblem', async (req, res, next)=> {
+  let {
+    name,
+    problem
+  }=req.body;
+  // let name = req.body.name;
+  // let psw = req.body.psw;
+   await plus('problem', [{
+    name,
+    problem
+    // name:name,
+    // psw:psw
+  }])
+  let data = await find ('problem')
+  res.send({
+    sucess:true,
+    data
+  });
+});
+
+
+// router.get('/test',async function (req, res, next) {
+//   // res.append('Access-Control-Allow-Origin', '*');
+//     let data = await find('goodlist', {});
+//     res.send(data);
+// });
+// /* 登录*/ 
+// router.post('/login', async function (req, res, next) {
+//   // res.append('Access-Control-Allow-Origin', '*');
+//   let name = req.body.name;
+//   let psw = req.body.psw;
+//   // console.log(req.body);
+//   let data = await find('sign', {
+//     name,
+//     psw
+//   });
+//   // console.log(data);
+//   res.send(data);
+// });
+// /*注册*/
+// router.post('/reg', async function (req, res, next) {
+//   let name = req.body.name;
+//   let psw = req.body.psw;
+//   let data = await plus('sign', {
+//     name:name,
+//     psw:psw
+//   })
+//   res.send(data);
+// });
+// //查找商品
+// router.get('/stock',async function (req, res, next) {
+//   // res.append('Access-Control-Allow-Origin', '*');
+//     let data = await find('stock', {});
+//     res.send(data);
+// });
+module.exports = router;
